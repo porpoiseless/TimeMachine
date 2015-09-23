c70b80c5b1a0c6f6b5c8aea0b52b335aa096497d
porpoiseless <jacoboen@Pacifica.attlocal.net>
2015-09-22 17:19:12 -0700

    Preliminary fix for synching problems with TimeMachine object.

--- tmAngular.js
+++ tmAngular.js
@@ -1,20 +1,57 @@
-// // -*- mode: js2 -*-
-// // Author: jto
+// -*- mode: js2 -*-
+// Author: jto
 
+// ===============
+// TIME MACHINE
+// ===============
+function TimeMachine(depth,speed){
+    this.depth = depth || 0;	// years before present
+    this.speed = speed || 0;	// years per jump
+};
+// things time machines can do
+// ========== TIMEJUMP!
+TimeMachine.prototype.jump = function(crashFn) { // fn to run on illegal jump
+    var proposedYear = parseInt(this.depth) + parseInt(this.speed);
+    if (proposedYear < 0) {	// check if visit is illegal, i.e. in real future
+	this.depth = 0;
+	this.speed = 0;
+	crashFn();
+    } else {			// if the jump is legal
+	this.depth = proposedYear; // do it
+    };
+};
+// ========== convert depth to formatted year
+TimeMachine.prototype.getYear = function(yearsBeforePresent) {
+    // turn years before present to a common-era style date
+    var ybp = parseInt(yearsBeforePresent) || this.depth; // default to time machine depth
+    var y = new Date().getFullYear() - ybp;
+    var suffix;		// for CE/BCE
+    if (y > 0){
+	suffix = " CE";
+    } else {
+	y = 1 + Math.abs(y); // bce format date, non-negative, starts @ 1
+	suffix = " BCE";
+    }
+    return y + suffix;
+};
+// ========== change speed
+TimeMachine.prototype.setSpeed = function(newSpeed) {
+    this.speed = newSpeed;
+};
+// ===============
+// ANGULAR.JS APP
+// ===============
 var app = angular.module('timeMachineAdventure', []);
 app.controller('tmControl', function($scope, $interval) {
-    $scope.tmSpeed = 0;		// initial speed 0... err, 1s/s?
-    $scope.tmYear = new Date().getFullYear(); // initial date: present
-    var updateYear = function(){
-	var theDestination = parseInt($scope.tmSpeed) + parseInt($scope.tmYear);
-	var timeHorizon = new Date().getFullYear().valueOf();
-	if (theDestination > timeHorizon) {
-	    $scope.tmSpeed = 0;
-	    alert("Warning! There are no chronoports on the future side of year " + timeHorizon);
-	    $scope.tmYear = timeHorizon;
-	} else {
-	    $scope.tmYear = theDestination;
-	};
+    $scope.speed = 0;
+    $scope.tm = new TimeMachine(10,0);
+    function update() {
+	$scope.tm.setSpeed($scope.speed);
+	$scope.tm.jump(function(){ //function to execute if illegal jump is made
+	    $scope.speed = 0;	   // has to be a call-back to access $scope
+	    alert("Warning! There are no chronoports on the future side of " + $scope.tm.getYear());
+	} );	    
     };
-    $interval(updateYear,1000);
+    
+    $interval(update,1000);
 });
